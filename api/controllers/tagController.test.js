f3db5fd525024a84a938fd34ebb7c765ffcee811
dapperauteur <a@awews.com>
2019-02-21 15:40:59 -0700

    add testing dependencies, create integration set up file, add script to start test to package.json

---
+++ api/controllers/tagController.test.js
@@ -0,0 +1,14 @@
+const { assert, expect } = require("chai");
+const Tag = require("./../models/tag");
+const tagController = require("./tagController");
+
+console.log("Tag");
+console.log(Tag);
+console.log("tagController");
+console.log(tagController);
+
+describe("Tag", () => {
+  it("confirm tests are working", () => {
+    expect(1).to.equal(1);
+  });
+});
