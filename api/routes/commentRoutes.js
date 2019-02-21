7add593fbe770124b072067f5f63bc3ff88da712
dapperauteur <a@awews.com>
2019-02-21 10:19:59 -0700

    add comment controller that talks to db

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,36 +1,15 @@
 const express = require("express");
+const commentsController = require("./../controllers/commentController");
 const router = express.Router();
 
-router.get("/", function(req, res) {
-  res.json(["array", "of", "comments"]);
-});
+router.get("/", commentsController.getComments);
 
-router.get("/:commentId", function(req, res) {
-  res.json({
-    title: "comment title",
-    commentDescription: "description of comment",
-    currentUserId: 2
-  });
-});
+router.get("/:commentId", commentsController.getComment);
 
-router.post("/", function(req, res) {
-  res.json({
-    title: "new comment title ",
-    commentDescription: "new description of comment",
-    currentUserId: 4
-  });
-});
+router.post("/", commentsController.createComment);
 
-router.put("/:commentId", function(req, res) {
-  res.json({
-    title: "updated comment title ",
-    commentDescription: "updated description of comment",
-    currentUserId: 5
-  });
-});
+router.put("/:commentId", commentsController.updateComment);
 
-router.delete("/:commentId", function(req, res) {
-  res.json({ message: "comment deleted" });
-});
+router.delete("/:commentId", commentsController.deleteComment);
 
 module.exports = router;
