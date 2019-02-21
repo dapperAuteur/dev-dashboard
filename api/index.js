cb48a5f762190291679506c94e1cea5d437c4974
dapperauteur <a@awews.com>
2019-02-20 19:25:32 -0700

    add get '/tags' route

--- api/index.js
+++ api/index.js
@@ -5,9 +5,38 @@ const express = require("express"),
 
 const db = require("./models");
 
+const tagRoutes = require("./routes/tagRoutes");
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// start example route
+const cb0 = function(req, res, next) {
+  console.log("CB0");
+  next();
+};
+
+const cb1 = function(req, res, next) {
+  console.log("CB1");
+  next();
+};
+
+app.get(
+  "/example/d",
+  [cb0, cb1],
+  function(req, res, next) {
+    console.log("the response will be sent by the next function...");
+    next();
+  },
+  function(req, res) {
+    res.send("Hello from D!");
+  }
+);
+
+// end example routes
+
+app.use("/tags", tagRoutes);
 
 const PORT = process.env.PORT || 8081;
 
