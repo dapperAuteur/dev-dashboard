351299ed8a4a82d2844a5fb6bc2102f8c482a7db
Esteban <sttrevino2@gmail.com>
2019-02-20 21:04:28 -0700

    added login and register routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const db = require('../models');
 
+//test
 router.get('/test', (req, res) => {
   res.json({
     message: 'user route works'
   });
 });
 
-router.post('/create', (req, res) => {
+router.post('/register', (req, res) => {
   db.User.create({ username: 'test789@test.com', password: 'test123' })
     .then(user => {
+      req.session.userid = user._id;
       res.status(200).json({
-        user: user,
         message: 'user saved successfully'
       });
     })
@@ -21,6 +23,24 @@ router.post('/create', (req, res) => {
         error: err
       });
     });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+  db.User.findOne({ username: username }).then(user => {
+    if (!user) {
+      return res.status(404).json({ message: 'Invalid Username' });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        req.session.userid = user._id;
+        res.status(200);
+      } else {
+        return res.status(404).json({ message: 'Incorrect Password' });
+      }
+    });
+  });
 });
 
 module.exports = router;
