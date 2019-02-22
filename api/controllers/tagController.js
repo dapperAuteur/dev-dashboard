3cdfcec0b10b1f5eacd394ad017370561b46bff6
dapperauteur <a@awews.com>
2019-02-22 09:53:08 -0700

    add tag controller and refactor routes

--- api/controllers/tagController.js
+++ api/controllers/tagController.js
@@ -1,6 +1,34 @@
 const db = require("./../models");
 const Tag = db.Tag;
 
+exports.getTags = function(req, res) {
+  Tag.find()
+    .then(function(tags) {
+      res.json({ tags });
+    })
+    .catch(function(err) {
+      res.json({ error: err });
+    });
+  /*
+    res.json([
+      { time: Date.now() },
+      { tagName: "javaScript", tagDescription: "what am I about" },
+      { tagName: "ReactJS", tagDescription: "Am I a framework or library?" },
+      { tagName: "recursion", tagDescription: "are you looking for recursion" }
+    ]);
+  */
+};
+
+exports.getTag = function(req, res) {
+  Tag.find(req.params.tagId)
+    .then(function(foundTag) {
+      res.json({ foundTag });
+    })
+    .catch(function(err) {
+      res.json({ error: err });
+    });
+};
+
 exports.createTag = function(req, res) {
   // let newTag = req.body;
   Tag.create(req.body)
@@ -15,6 +43,30 @@ exports.createTag = function(req, res) {
   // newTag.tagDescription = "what am I about";
   // console.log("newTag", newTag);
   // res.json(newTag);
+};
+
+exports.updateTag = function(req, res) {
+  Tag.findOneAndUpdate({ _id: req.params.tagId }, req.body, {
+    new: true
+  })
+    .then(function(updatedTag) {
+      res.status(201).json({ updatedTag });
+    })
+    .catch(function(err) {
+      res.json({ error: err });
+    });
+};
+
+exports.deleteTag = function(req, res) {
+  Tag.remove({ _id: req.params.tagId })
+    .then(function() {
+      res
+        .status(201)
+        .json({ message: `comment with ${req.params.tagId} deleted` });
+    })
+    .catch(function(err) {
+      res.json({ error: err });
+    });
 };
 
 module.exports = exports;
--- api/routes/tagRoutes.js
+++ api/routes/tagRoutes.js
@@ -1,40 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const tagController = require("./../controllers/tagController");
 
-router.get("/", function(req, res) {
-  res.json([
-    { time: Date.now() },
-    { tagName: "javaScript", tagDescription: "what am I about" },
-    { tagName: "ReactJS", tagDescription: "Am I a framework or library?" },
-    { tagName: "recursion", tagDescription: "are you looking for recursion" }
-  ]);
-});
+router.get("/", tagController.getTags);
 
-router.get("/:tagId", function(req, res) {
-  res.json({ tagName: "javaScript", tagDescription: "what am I about" });
-});
+router.get("/:tagId", tagController.getTag);
 
-router.post("/", function(req, res) {
-  // let newTag = req.body.newTag;
-  let newTag = req.body;
-  newTag.tagName = "new tag";
-  newTag.tagDescription = "what am I about";
-  console.log("newTag", newTag);
-  res.json(newTag);
-});
+router.post("/", tagController.createTag);
 
-router.put("/:tagId", function(req, res) {
-  // let updatedTag = req.body.updatedTag;
-  let updatedTag = req.body;
-  updatedTag.tagName = "updated tag";
-  updatedTag.tagDescription = "what am I about";
-  console.log("updatedTag", updatedTag);
-  res.json(updatedTag);
-});
+router.put("/:tagId", tagController.updateTag);
 
-router.delete("/:tagId", function(req, res) {
-  let tagId = "2";
-  res.json({ message: `tag with ${tagId} deleted` });
-});
+router.delete("/:tagId", tagController.deleteTag);
 
 module.exports = router;
