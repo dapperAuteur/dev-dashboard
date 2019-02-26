0e43e33e8e37c5c7efa2c62fe8a03ffb5ce69a0b
dapperauteur <a@awews.com>
2019-02-26 12:21:49 -0700

    add ducks back to ui

---
+++ ui/src/ducks/reducer.js
@@ -0,0 +1,34 @@
+import { VERIFIED_USER } from "./actions";
+
+const INITIAL_STATE = {
+  user: null,
+  picUrl: ""
+};
+
+const USER_LOGIN = "USER_LOGIN";
+const USER_PHOTO = "USER_PHOTO";
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case USER_LOGIN:
+      return { ...state, user: action.payload };
+    case USER_PHOTO:
+      return { ...state, picUrl: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function userLogin(user) {
+  return {
+    type: USER_LOGIN,
+    payload: user
+  };
+}
+
+export function userPhoto(picUrl) {
+  return {
+    type: USER_PHOTO,
+    payload: picUrl
+  };
+}
