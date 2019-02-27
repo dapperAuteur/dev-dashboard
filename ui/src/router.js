325fa9a217d7a17c950c89b7510648a9966faa9f
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2019-02-27 15:08:00 -0700

    codereview

--- ui/src/router.js
+++ ui/src/router.js
@@ -1,17 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Switch, Route } from "react-router-dom";
 import SecureRoute from "./secureUtils/SecureRoute";
 import Issue from "./components/Issue/Issue";
 import AddIssue from "./components/AddIssue/AddIssue";
 import Home from "./components/Home/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Profile from "./components/Profile/Profile";
-import Register from "./components/Auth/Register";
+import Register from "./components/Auth/register";
 import Login from "./components/Auth/Login";
 
 export default (
